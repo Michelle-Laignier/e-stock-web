ad85d79044f81d098b5604965fa2805691e2f118
Laignier <michellelaignier17@gmail.com>
2023-10-26 19:02:52 -0300

    cookie na autorizacao

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,12 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: 'http://localhost:3333'
 });
+
+/* pra fazer um withCredentials geral:
+
+export const api = axios.create({
+  baseURL: 'http://localhost:3333',
+  withCredentials: true
+});
+
+*/
